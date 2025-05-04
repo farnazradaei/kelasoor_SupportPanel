a5de9677621c56df1b9abcf8644976b9985776aa
farnazradaei <farnaz.radaei@gmail.com>
2025-05-04 20:28:31 +0330

    user serializers

---
+++ apps/users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['phone_number', 'first_name',' last_name', 'national_id', 'gender']
+        read_only_fiels = ['role']
+
