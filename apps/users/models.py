317561f529fcec822b4ed2090a7f3c97bca77cb4
farnazradaei <farnaz.radaei@gmail.com>
2025-05-10 19:09:02 +0330

    added changes in models and apps

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,36 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager , Group
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
+from django.utils import timezone
 
+class UserManager(BaseUserManager):
+   def get_by_natural_key(self, phone_number):
+    return self.get(phone_number=phone_number)
+   def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+   def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class User (AbstractBaseUser , PermissionsMixin):
+    
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    
     Gender_choices = (
         ('female' , 'زن') ,
         ('male' , 'مرد') , 
@@ -22,6 +48,11 @@ class User (AbstractBaseUser , PermissionsMixin):
     national_id = models.CharField(max_length=10 , unique=True)
     gender = models.CharField(max_length=10 , choices=Gender_choices)
     role = models.CharField(max_length=20, choices=ROLE_CHOICE , default='user'  , editable=False)
+    is_staff = models.BooleanField(default=False)  
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
 
     def save(self , *args , **kwargs):
         super().save(*args , **kwargs)
@@ -33,3 +64,13 @@ class User (AbstractBaseUser , PermissionsMixin):
             self.groups.set([group])
         else:
             self.groups.clear()
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def is_valid(self):
+        return timezone.now() <= self.expires_at
