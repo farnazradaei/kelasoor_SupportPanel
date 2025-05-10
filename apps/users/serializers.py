a4d3c5145cf6ca033f2dc8c96a4870105b9d01f0
farnazradaei <farnaz.radaei@gmail.com>
2025-05-10 19:05:50 +0330

    serializer update

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from models import User
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['phone_number', 'first_name',' last_name', 'national_id', 'gender']
+        fields = ['phone_number', 'first_name','last_name', 'national_id', 'gender']
         read_only_fiels = ['role']
+        extra_kwargs = {
+            'phone_number': {'required': True},
+            'password': {'write_only': True},
+        }
 
+def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
