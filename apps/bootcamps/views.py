28dd24e0cd20ddf3d89093abcc9b6d2b44fd73dd
farnazradaei <farnaz.radaei@gmail.com>
2025-05-21 20:38:16 +0330

    BootcampCategory

--- apps/bootcamps/views.py
+++ apps/bootcamps/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from models import Bootcamp , BootcampCategory
+from .serializers import Bootcampserializer , BootcampCategorySerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+
+class BootcampViewSet(viewsets.ModelViewSet):
+    queryset = Bootcamp.objects.filter(type=Bootcamp.BootcampType.NORMAL)
+    serializer_class = Bootcampserializer
+    permission_classes = [IsAuthenticated , IsAdminOrReadOnly]
+
+
+class BootcampCategoryViewSet(viewsets.ModelViewSet):
+    queryset = BootcampCategory.objects.all()
+    serializer_class = BootcampCategorySerializer
+    permission_classes = [IsAuthenticated ,  IsAdminOrReadOnly]
+
+
+class AdvancedBootcampViewSet(viewsets.ModelViewSet):
+    queryset = Bootcamp.objects.filter(Bootcamp.BootcampType.ADVANCED)
+    serializer_class = Bootcampserializer
+    permission_classes = [IsAuthenticated , IsAdminOrReadOnly]
+
