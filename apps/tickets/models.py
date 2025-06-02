fefe53933dbe97dbaa6cee5ebebe3a9cf77f258f
farnazradaei <farnaz.radaei@gmail.com>
2025-06-02 16:36:36 +0330

    tickets models

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    ...
+    class Meta:
+        permissions = [
+            ('view_ticket', 'Can view ticket'),
+            ('answer_ticket', 'Can answer ticket'),
+            ('edit_ticket', 'Can change ticket'),
+            ('delete_ticket', 'Can delete ticket'),
+        ]
