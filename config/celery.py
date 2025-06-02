800288c6fbc31937784df664371ac0217d2421a4
farnazradaei <farnaz.radaei@gmail.com>
2025-06-02 16:29:48 +0330

    requirements

--- config/celery.py
+++ config/celery.py
@@ -2,8 +2,8 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTING_MODULE','your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('kelasoor_SupportPanel')
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
