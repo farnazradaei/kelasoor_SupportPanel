a9c991103f742cc211bda159bb4af448ea28df1f
farnazradaei <farnaz.radaei@gmail.com>
2025-06-02 18:12:35 +0330

    Bootcamp Views Completed

--- apps/bootcamps/views.py
+++ apps/bootcamps/views.py
@@ -1,55 +1,75 @@
 from rest_framework import viewsets , generics , permissions
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
-from models import Bootcamp , BootcampCategory ,BootcampRegistration  
-from .serializers import Bootcampserializer , BootcampCategorySerializer ,BootcampRegistrationSerializer
+from .models import Bootcamp, BootcampCategory, BootcampRegistration, BootcampRole
 from .permissions import IsAdminOrReadOnly 
 from django.core.exceptions import ValidationError
-
-
-
-class BootcampViewSet(viewsets.ModelViewSet):
-    queryset = Bootcamp.objects.filter(type=Bootcamp.BootcampType.NORMAL)
-    serializer_class = Bootcampserializer
-    permission_classes = [IsAuthenticated , IsAdminOrReadOnly]
+from .serializers import (
+    BootcampSerializer ,
+    BootcampCategorySerializer ,
+    BootcampRegistrationSerializer ,
+    BootcampRoleCreateSerializer ,
+    BootcampRoleSerializer)
 
 
 class BootcampCategoryViewSet(viewsets.ModelViewSet):
     queryset = BootcampCategory.objects.all()
     serializer_class = BootcampCategorySerializer
-    permission_classes = [IsAuthenticated ,  IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+
+class BootcampViewSet(viewsets.ModelViewSet):
+    queryset = Bootcamp.objects.filter(type=Bootcamp.BootcampType.NORMAL)
+    serializer_class = BootcampSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
 
 class AdvancedBootcampViewSet(viewsets.ModelViewSet):
-    queryset = Bootcamp.objects.filter(Bootcamp.BootcampType.ADVANCED)
-    serializer_class = Bootcampserializer
-    permission_classes = [IsAuthenticated , IsAdminOrReadOnly]
+    queryset = Bootcamp.objects.filter(type=Bootcamp.BootcampType.ADVANCED)
+    serializer_class = BootcampSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
 
 
 class BootcampRegistrationCreateView(generics.CreateAPIView):
     queryset = BootcampRegistration.objects.all()
     serializer_class = BootcampRegistrationSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
-        Bootcamp = serializer.validated_data['bootcamp']
-        if Bootcamp.status != Bootcamp.StatusChoices.OPEN:
-            raise ValidationError("sabatnam dar in bootcamp faal nist")
+        bootcamp = serializer.validated_data['bootcamp']
+        if bootcamp.status != Bootcamp.StatusChoices.REGISTRATION:
+            raise ValidationError("ثبت‌نام در این بوتکمپ فعال نیست")
         serializer.save()
 
-class BootcampRegistrationUpdateStatusView(generics.UpdateAPIView):
+class BootcampRegistrationStatusUpdateView(generics.UpdateAPIView):
     queryset = BootcampRegistration.objects.all()
     serializer_class = BootcampRegistrationSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+    permission_classes = [IsAdminUser]
 
     def update(self, request, *args, **kwargs):
-        isinstance =  self.get_object()
-        new_staus = request.data.get("status")
+        instance = self.get_object()
+        new_status = request.data.get("status")
 
-        if new_staus not in dict(BootcampRegistration.statuschoices.choices):
-            return Response({"detail :vaziiat namotabar ast"}, status = 400 )
-        
-        isinstance.status = new_staus
-        isinstance.save()
+        if new_status not in dict(BootcampRegistration.statuschoices.choices):
+            return Response({"detail": "وضعیت نامعتبر است"}, status=400)
 
-        
+        instance.status = new_status
+        instance.save()
+        return Response({"detail": "وضعیت با موفقیت بروزرسانی شد"})
+
+
+class BootcampRoleCreateView(generics.CreateAPIView):
+    queryset = BootcampRole.objects.all()
+    serializer_class = BootcampRoleCreateSerializer
+    permission_classes = [IsAdminUser]
+
+
+class BootcampRoleListView(generics.ListAPIView):
+    serializer_class = BootcampRoleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        bootcamp_id = self.kwargs['bootcamp_id']
+        return BootcampRole.objects.filter(bootcamp__id=bootcamp_id)
