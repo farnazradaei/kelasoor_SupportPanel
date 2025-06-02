8fcc274dbc9addc03690d6a24e27f86870d6d6e4
farnazradaei <farnaz.radaei@gmail.com>
2025-06-02 16:37:18 +0330

    blog models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    ...
+    class Meta:
+        permissions = [
+            ('add_text', 'Can add text'),
+            ('edit_text', 'Can edit text'),
+            ('delete_text', 'Can delete text'),
+            ('add_file' , 'can add file')
+        ]
